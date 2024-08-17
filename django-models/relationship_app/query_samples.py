afa04edec588a6e7a2c3f8b23aed4c7822f173d1
Ndumiso <ndumisovee@gmail.com>
2024-08-18 01:55:43 +0200

    file updated

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = books = Book.objects.filter(author=author_name)  # Using related_name 'books' from the ForeignKey
+    books = Book.objects.filter(author=author_name)  # Using related_name 'books' from the ForeignKey
     return books
 
 

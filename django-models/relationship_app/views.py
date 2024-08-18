50db5e62618b817830afc877f123b295f3e3e030
Ndumiso <ndumisovee@gmail.com>
2024-08-18 02:40:07 +0200

    file updated

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Book
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.views import View

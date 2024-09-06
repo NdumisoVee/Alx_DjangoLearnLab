5f0bbcc3e1e283b18108a18a0f68962ead238e2f
Ndumiso <ndumisovee@gmail.com>
2024-09-06 16:15:41 +0200

    file updated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics
+from api.models import Book
+from .serializers import BookSerializer
+from rest_framework import filters
 
 # Create your views here.
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author']
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+# BookListView: Returns a list of all books in the database.
+# BookDetailView: Returns details of a single book identified by the primary key (ID).
+# BookCreateView: Allows authenticated users to add a new book.
+# BookUpdateView: Allows authenticated users to update an existing book.
+# BookDeleteView: Allows authenticated users to delete a book.

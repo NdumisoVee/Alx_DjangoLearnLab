55dfc5ca71032eccd138211a84fab8f54fddf243
unknown <admin@majopuwff4pc20.majuba.edu.za>
2024-09-09 10:37:09 +0200

    file updated

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,24 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from django.utils import timezone
-from django.shortcuts import get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic.edit import UpdateView, DeleteView
-from .models import Post, Comment
-from .forms import CommentForm
-
-
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
 
 
 class Post(models.Model):
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Blog_DB',
+        'USER': 'Admin',
+        'PASSWORD': 'Admin23',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -121,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

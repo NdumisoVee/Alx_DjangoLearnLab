b2e7b93ef07b10d3efd923469e0d1fe4c9758ee3
unknown <admin@majopuwff4pc20.majuba.edu.za>
2024-09-09 12:29:02 +0200

    updated urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:pk>/comment/new/', add_comment, name='add-comment'),
+    path('post/<int:pk>/comments/new/', add_comment, name='add-comment'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update-comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 

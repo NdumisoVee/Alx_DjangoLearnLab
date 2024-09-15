efb876913198c6e3578141ede19c3839b1c8ab0d
Ndumiso <ndumisovee@gmail.com>
2024-09-16 00:07:44 +0200

    updated urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update-comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
     path('search/', views.search_posts, name='search_posts'),
-    path('tags/<slug:slug>/', PostByTagListView.as_view(), name='posts_by_tag'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts_by_tag'),
 
 
 ]
